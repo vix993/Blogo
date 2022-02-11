4f6d1bfc2adb4a67934f044f44805bd8239bcc62
Victor Nascimento <victornascimento@Victors-MacBook-Pro.local>
2022-02-11 14:26:44 -0300

    fix: svg properties

--- src/components/InputFilter.tsx
+++ src/components/InputFilter.tsx
@@ -14,7 +14,7 @@ export const InputFilter: React.FC<InputFilterProps> = ({}) => {
                 <div className="relative text-stone-200 focus-within:text-stone-800">
                 <span className="absolute inset-y-0 left-0 flex items-center pl-2">
                     <button type="submit" className="p-1 focus:outline-none focus:shadow-outline">
-                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" className="w-6 h-6"><path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+                    <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" className="w-6 h-6"><path d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
                     </button>
                 </span>
                 <input
